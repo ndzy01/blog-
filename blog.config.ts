514549492e64e42eb5ae5335fb290156ed43b264
zhang yi <2326962137@qq.com>
2020-04-05 08:42:52 +0800

    第一次提交

---
+++ blog.config.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: ndzy
+ * @Date: 2020-04-05 07:11:13
+ * @LastEditTime: 2020-04-05 08:29:12
+ * @LastEditors: ndzy
+ */
+import { IBlogConfig } from '@/types'
+
+const config: IBlogConfig = {
+  host: '0.0.0.0',
+  port: 9000,
+  enableHTTPS: false,
+  mongoUrl: 'mongodb://root:root@localhost:28888/blog?authSource=admin',
+  jwtSecret: 'myblogjsonwebtokensecretkey',
+}
+
+export default config
